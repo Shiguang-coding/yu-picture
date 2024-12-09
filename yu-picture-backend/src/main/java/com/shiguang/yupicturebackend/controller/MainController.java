a6159b75034ffaa96dd73a9a4ee6e7d6f68fea79
shiguang <an_shiguang@163.com>
2024-12-10 00:04:55 +0800

    第二期：后端项目初始化

---
+++ yu-picture-backend/src/main/java/com/shiguang/yupicturebackend/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.shiguang.yupicturebackend.controller;
+
+import com.shiguang.yupicturebackend.common.BaseResponse;
+import com.shiguang.yupicturebackend.common.ResultUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+
+    /**
+     * 健康检查
+     */
+    @GetMapping("/health")
+    public BaseResponse<String> health() {
+        return ResultUtils.success("ok");
+    }
+}
